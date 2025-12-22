c7afd2fc259e1839ecde42383d2bdb31c4d6cc30
Matjaz Muhic <matjazvn@gmail.com>
2025-12-22 21:52:47 +0100

    Adds hosts to allow testing devices to access the server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts = [ "localhost", "mono-ub-dev" ]
 
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
